7803470c1c8bbba24fc8384fd5268f7e2b0a2fac
rkp8000 <rpang.contact@gmail.com>
2017-07-25 11:25:18 -0700

    Add function to animate trajectory.

--- animation/__init__.py
+++ animation/__init__.py
@@ -2,14 +2,45 @@ import imageio
 import os
 
 
-def create_gif(files, save_prefix, fps):
+def create_gif(files, save_file, playback_fps, delete_originals=False, verbose=False):
     """
     Create a gif from a sequence of image files.
-    """
     
+    :param files: ordered list of paths to files to be merged into gif
+    :param save_file: prefix (without ".gif") of file to save gif to
+    :param playback_fps: fps of gif given files (max 30 Hz)
+    :param delete_originals: set to True to delete files used to make original gif
+    """
+          
+    if playback_fps > 30:
+        print('Warning: playback rates > 30 Hz are usually rendered incorrectly')
+        
     # make sure save directory exists
-    save_dir = os.path.dirname(save_prefix)
+    save_dir = os.path.dirname(save_file)
     if not os.path.exists(save_dir): os.makedirs(save_dir)
         
+    if verbose:
+        print('Loading source images...')
+        
     images = [imageio.imread(file) for file in files]
-    imageio.mimsave(save_prefix + '.gif', images, fps=fps)
+    
+    if verbose:
+        print('Source images loaded.')
+        print('Generating gif...')
+        
+    imageio.mimsave(save_file + '.gif', images, fps=playback_fps)
+    
+    if verbose:
+        print('Gif generated.')
+    
+    if delete_originals:
+        if verbose:
+            print('Deleting originals...')
+            
+        for file in files:
+            os.remove(file)
+            
+        if verbose:
+            print('Originals deleted.')
+            
+    return save_file + '.gif'
--- animation/build_frames.py
+++ animation/build_frames.py
@@ -1,13 +1,79 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from scipy import signal
 import shelve
 
 
-def network_activity(
-        save_prefix, time_file, activity_file, fps=None, resting_size=50, spiking_size=1000,
-        default_color=(0, 0, 0), spiking_color=(1, 0, 0), box=(-1, 1, -1, 1), fig_size=(7, 7),
-        verbose=False):
+def downsample_spikes(spikes, num):
+    """
+    Downsample a vector spike train to have num equally spaced samples.
+    The downsampled spike value at a given time point is True if any spike occurred
+    in the window surrounding that time point, and zero otherwise.
+    
+    :param spikes: 2-D logical array indicating spike times (rows are times, cols
+        are neurons)
+    :param num: number of time points in the resampled signal
+    """
+    window = len(spikes) / num
+    
+    if window < 1:
+        err_msg = ('Provided "num" value must be less than len(spikes); '
+                   'upsampling is not supported')
+        raise ValueError(err_msg)
+    
+    # just use a loop for now
+    
+    spikes_down = np.zeros((num, spikes.shape[1]), dtype=bool)
+    
+    for f_ctr in range(num):
+        
+        # get start and end of window for this downsampled time point
+        start = int(round(window * f_ctr))
+        end = int(round(window * (f_ctr + 1)))
+        
+        # assign the downsampled value to True for each neuron in which
+        # any spike occurred in this time window
+        spikes_down[f_ctr] = np.any(spikes[start:end], axis=0)
+    
+    return spikes_down
+
+
+def downsample_ma(xs, num):
+    """
+    Downsample an array to have num equally spaced samples, where the downsampled
+    value at each time point is a moving average of the values in the corresponding window.
+    
+    :param xs: 2-D array of values (rows are times, cols are variables)
+    :param num: number of time points in the resampled signal
+    """
+    window = len(xs) / num
+    
+    if window < 1:
+        err_msg = ('Provided "num" value must be less than len(xs); '
+                   'upsampling is not supported')
+        raise ValueError(err_msg)
+    
+    # just use a loop for now
+    xs_down = np.nan * np.zeros((num,) + xs.shape[1:])
+    
+    for f_ctr in range(num):
+        
+        # get start and end of window for this downsampled time point
+        start = int(round(window * f_ctr))
+        end = int(round(window * (f_ctr + 1)))
+        
+        # assign the downsampled value to the average of the values in
+        # the corresponding window
+        xs_down[f_ctr] = np.mean(xs[start:end], axis=0)
+    
+    return xs_down
+
+
+def ntwk_activity(
+        save_prefix, time_file, activity_file, fps=30, resting_size=50, spiking_size=1000,
+        default_color=(0, 0, 0), spiking_color=(1, 0, 0), frames_per_spike=5,
+        box=None, show_timestamp=True, fig_size=(7, 7), verbose=False):
     """
     Convert a time-series of membrane potentials and spikes into viewable frames.
     
@@ -29,16 +95,30 @@ def network_activity(
     :param default_color: neuron color
     :param spiking_color: color of neuron when spiking
     :param box: bounding box to display neurons in: (x_min, x_max, y_min, y_max)
+    :param show_timestamp: whether or not to show timestamp in figure title
     :param fig_size: size of figure to make
+    :param verbose: whether or not to print progress details
     """
     
+    if verbose:
+        print('Using timestamp file "{}" and activity file "{}".'.format(time_file, activity_file))
+        print('Frames will be saved with prefix "{}".'.format(save_prefix))
+        print('Loading timestamps and network activity data...')
+        
     # load time stamps
     data_t = shelve.open(time_file)
     
-    if 'timestamps' not in data_t:
-        raise KeyError('Item with key "timestamps" not found in file "{}".'.format(time_file))
+    for key in ('timestamps', 'fs'):
+        if key not in data_t:
+            raise KeyError('Item with key "{}" not found in file "{}".'.format(key, time_file))
         
     ts = data_t['timestamps']
+    fs = data_t['fs']
+    
+    if fps > fs:
+        err_msg = ('Provided "fps" value must be smaller than original sampling frequency; '
+               'upsampling is not supported.')
+        raise ValueError(err_msg)
     
     # load activity data
     data_a = shelve.open(activity_file)
@@ -52,14 +132,50 @@ def network_activity(
     v_rest = data_a['v_rest']
     v_th = data_a['v_th']
     
+    # make sure timestamp vector is same length as activity vectors
+    assert len(ts) == len(vs) == len(spikes)
+    
+    if verbose:
+        print('Data loaded.')
+        
+    # downsample data if necessary
+    if fps < fs:
+        
+        if verbose:
+            print('Downsampling data from {} Hz to {} fps...'.format(fs, fps))
+        
+        # membrane potential
+        n_down = int(round((ts[-1] - ts[0]) * fps))
+        vs, ts = signal.resample(vs, n_down, t=ts, axis=0)
+        
+        # spikes
+        spikes = downsample_spikes(spikes, n_down)
+    
+        if verbose:
+            print('Data downsampled.')
+    
     # extract positions from data or generate random ones
     if 'positions' in data_a:
         positions = data_a['positions']
     else:
         positions = np.random.normal(0, 1, (2, vs.shape[1]))
-    
-    # make sure timestamp vector is same length as activity vectors
-    assert len(ts) == len(vs) == len(spikes)
+        
+    # automatically compute box size if not provided
+    if box is None:
+        x_min = positions[0].min()
+        x_max = positions[0].max()
+        x_r = x_max - x_min
+        
+        y_min = positions[1].min()
+        y_max = positions[1].max()
+        y_r = y_max - y_min
+        
+        box = [
+            x_min - 0.1*x_r,
+            x_max + 0.1*x_r,
+            y_min - 0.1*y_r,
+            y_max + 0.1*y_r,
+        ]
     
     # convert membrane potentials to scatter sizes
     slope = (spiking_size - resting_size) / (v_th - v_rest)
@@ -72,25 +188,160 @@ def network_activity(
     # set up figure and position neurons
     fig, ax = plt.subplots(1, 1)
     
-    ax.set_xlim(*box[:2])
-    ax.set_ylim(*box[2:])
+    ax.set_xlim(box[:2])
+    ax.set_ylim(box[2:])
     
     sca = ax.scatter(positions[0], positions[1], c=default_color, s=10, lw=0)
     
     # loop over frames
+    if verbose:
+        print('Generating and saving {} frames...'.format(len(ts)))
+        
     save_files = []
+    spike_offset_ctr = np.zeros(spikes.shape[1], dtype=int)
     
     for f_ctr, (t, sizes_, spikes_) in enumerate(zip(ts, sizes, spikes)):
         
-        # set sizes
-        sca.set_sizes(sizes_)
+        # set colors according to spiking
+        spike_offset_ctr[spikes_] = frames_per_spike
         
         # set colors
-        if not any(spikes_):
+        if not any(spike_offset_ctr):
             sca.set_color(default_color)
         else:
-            colors = [spiking_color if s else default_color for s in spikes_]
+            colors = [spiking_color if s else default_color for s in spike_offset_ctr]
             sca.set_color(colors)
+            
+        # set sizes of non-spiking neurons
+        sizes_[spike_offset_ctr > 0] = spiking_size
+        sca.set_sizes(sizes_)
+        
+        if show_timestamp:
+            ax.set_title('t = {0:.3f} s'.format(t))
+            
+        plt.draw()
+        
+        spike_offset_ctr[spike_offset_ctr > 0] -= 1
+        
+        save_file = '{}_{}.png'.format(save_prefix, f_ctr+1)
+        save_files.append(save_file)
+        
+        fig.savefig(save_file)
+        
+    plt.close()
+    
+    if verbose:
+        print('Frames saved.')
+        
+    return save_files
+
+
+def traj(
+        save_prefix, time_file, traj_file, fps=30, decay=0.5,
+        location_size=2000, path_size=200, location_color=(0, 0, 1, .3), path_color=(0, 0, 0),
+        box=None, fig_size=(7, 7), show_timestamp=True, verbose=False):
+    """
+    Convert a time-series of positions into a series of still frames.
+    
+    :param save_prefix: prefix of frame files
+    :param time_file: shelved file containing the following fields:
+        'timestamps': 1-D array containing timestamps corresponding to neural activity
+        'fs': scalar sampling frequency
+    :param traj_file: shelved file containing the following fields:
+        'xys': 2-D array containing (x, y) coordinates of each position over time; rows are
+            time points, cols are x and y
+    :param fps: frame rate
+    :param location_size: size of current location marker
+    :param path_size: size of markers indicating recent path
+    :param location_color: color of current location marker
+    :param path_color: color of markers indicating recent path (note: rgb, not rgba)
+    :param box: bounding box to display path in
+    :param show_timestamp: whether or not to show timestamp in figure title
+    :param fig_size: size of figure to make
+    :param verbose: whether or not to print progress details
+    """
+    
+    # load time stamps
+    data_t = shelve.open(time_file)
+    
+    for key in ('timestamps', 'fs'):
+        if key not in data_t:
+            raise KeyError('Item with key "{}" not found in file "{}".'.format(key, time_file))
+        
+    ts = data_t['timestamps']
+    fs = data_t['fs']
+    
+    # load trajectory data
+    data_tr = shelve.open(traj_file)
+    
+    if 'xys' not in data_tr:
+        raise KeyError('Item with key "{}" not found in file "{}".'.format('xys', trajectory_file))
+        
+    xys = data_tr['xys']
+        
+    # downsample data if necessary
+    if fps < fs:
+        
+        # positions
+        n_down = int(round((ts[-1] - ts[0]) * fps))
+        ts = downsample_ma(ts, n_down)
+        xys = downsample_ma(xys, n_down)
+    
+    # convert to two 1D arrays
+    xs, ys = xys.T
+
+    # automatically compute box size if not provided
+    if box is None:
+        x_min = xys[:, 0].min()
+        x_max = xys[:, 0].max()
+        x_r = x_max - x_min
+        
+        y_min = xys[:, 1].min()
+        y_max = xys[:, 1].max()
+        y_r = y_max - y_min
+        
+        box = [
+            x_min - 0.1*x_r,
+            x_max + 0.1*x_r,
+            y_min - 0.1*y_r,
+            y_max + 0.1*y_r,
+        ]
+        
+    # make sure save directory exists
+    save_dir = os.path.dirname(save_prefix)
+    if not os.path.exists(save_dir): os.makedirs(save_dir)
+        
+    # set up figure
+    fig, ax = plt.subplots(1, 1)
+    
+    ax.set_xlim(box[:2])
+    ax.set_ylim(box[2:])
+    
+    # plot all points with zero opacity
+    sca = ax.scatter(xs, ys, s=path_size, c=(path_color + (0,)), lw=0, zorder=0)
+    
+    # plot current position
+    sca_2 = ax.scatter(xs[0], ys[0], s=location_size, c=location_color, lw=0, zorder=1)
+    
+    # loop over frames
+    save_files = []
+    
+    for f_ctr, (t, (x, y)) in enumerate(zip(ts, xys)):
+        
+        # get opacities of trailing path
+        alphas = np.exp((ts - t)/decay)
+        alphas[ts > t] = 0
+        
+        # plot trailing path
+        colors = [path_color + (alpha,) for alpha in alphas]
+            
+        sca.set_color(colors)
+        
+        # plot current location
+        sca_2.set_offsets([x, y])
+        
+        if show_timestamp:
+            ax.set_title('t = {0:.3f} s'.format(t))
             
         plt.draw()
         
@@ -100,4 +351,5 @@ def network_activity(
         fig.savefig(save_file)
         
     plt.close()
+        
     return save_files
