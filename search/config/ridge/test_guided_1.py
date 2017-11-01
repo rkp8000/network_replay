529dce52297690d2dbc495960ce9a1ee39bd0b8e
rkp8000 <rpang.contact@gmail.com>
2017-10-31 17:08:31 -0700

    Fix bugs and run tests of objective function and search algorithm.

---
+++ search/config/ridge/test_guided_1.py
@@ -0,0 +1,62 @@
+SMLN_ID = 'test_guided'
+
+START = {
+    'RIDGE_H': 1, 'RIDGE_W': 2, 'P_INH': 1, 'RHO_PC': 1,
+    'Z_PC': 2, 'L_PC': 1, 'W_A_PC_PC': 2,
+    'P_A_INH_PC': 1, 'W_A_INH_PC': 4,
+    'P_G_PC_INH': 5, 'W_G_PC_INH': 6,
+    'FR_EC': 9,
+}
+
+# ranges can be:
+#     3-element list, where first elements are lb, ub,
+#     and final element is scale (resolution)
+#
+#     1-element list specifying fixed param value
+
+P_RANGES = (
+    ('RIDGE_H', [0, 10, 1]),
+    ('RIDGE_W', [0, 10, 1]),
+    ('P_INH', [0, 10, 1]),
+    ('RHO_PC', [0, 10, 1]),
+        
+    ('Z_PC', [0, 10, 1]),
+    ('L_PC', [0, 10, 1]),
+    ('W_A_PC_PC', [0, 10, 1]),
+
+    ('P_A_INH_PC', [0, 10, 1]),
+    ('W_A_INH_PC', [4]),
+
+    ('P_G_PC_INH', [5]),
+    ('W_G_PC_INH', [6]),
+
+    ('FR_EC', [9]),
+)
+
+Q_JUMP = 0
+
+Q_NEW = 1
+
+SGM_RAND = 0.1
+
+A_PREV = 1
+B_PREV_Y = 1
+B_PREV_K = 0
+B_PREV_S = 1
+B_PREV_SUM = B_PREV_Y + B_PREV_K + B_PREV_S
+
+L_STEP = 0.1
+L_PHI = 1
+N_PHI = 5
+
+A_PHI = 5
+B_PHI_Y = 0
+B_PHI_K = 5
+B_PHI_S = 5
+B_PHI_SUM = B_PHI_Y + B_PHI_K + B_PHI_S
+
+K_TARG = 9
+S_TARG = 9
+
+ETA_K = 50
+ETA_S = 50
