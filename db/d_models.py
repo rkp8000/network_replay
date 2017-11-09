2c8c2880dd376ad09ec0e7d50d20b423755b0c87
rkp8000 <rpang.contact@gmail.com>
2017-11-08 17:56:08 -0800

    Add untested infrastructure for embedded-ridge-trials.

--- db/d_models.py
+++ db/d_models.py
@@ -54,3 +54,55 @@ class RidgeTrial(Base):
     stability = Column(Float)
     activity = Column(Float)
     speed = Column(Float)
+
+
+class EmbeddedSearcher(Base):
+    
+    __tablename__ = 'embedded_searcher'
+    
+    id = Column(Integer, primary_key=True)
+    
+    smln_id = Column(String)
+    role = Column(String)
+    last_active = Column(DateTime)
+    error = Column(String)
+    traceback = Column(String)
+    
+    commit = Column(String)
+    
+    
+class EmbeddedTrial(Base):
+    
+    __tablename__ = 'embedded_trial'
+    
+    id = Column(Integer, primary_key=True)
+    
+    searcher_id = Column(Integer, ForeignKey('embedded_searcher.id'))
+    searcher = relationship('EmbeddedSearcher', backref='trials')
+    
+    seed = Column(Integer)
+    
+    # parameters
+    area_h = Column(Float)
+    area_w = Column(Float)
+    ridge_y = Column(Float)
+    p_inh = Column(Float)
+    rho_pc = Column(Float)
+    
+    z_pc = Column(Float)
+    l_pc = Column(Float)
+    w_a_pc_pc = Column(Float)
+    
+    p_a_inh_pc = Column(Float)
+    w_a_inh_pc = Column(Float)
+    
+    p_g_pc_inh = Column(Float)
+    w_g_pc_inh = Column(Float)
+    
+    fr_ec = Column(Float)
+    
+    # results
+    stability = Column(Float)
+    angle = Column(Float)
+    activity = Column(Float)
+    speed = Column(Float)
---
+++ search/embedded_global.py
@@ -0,0 +1,64 @@
+"""
+GLOBAL CONFIG FOR RIDGE SEARCH AND OBJECTIVE FUNCTION.
+"""
+import numpy as np
+
+
+# PRE-COMPUTATION CONFIG
+
+SEED_PRE = 0
+
+
+## W_N_PC_EC VS DIST COMPUTATION
+
+PATH_W_N_PC_EC_VS_DIST = 'search/pre/ridge/w_n_pc_ec_vs_dist.npy'
+
+DUR_W_PRE = 5.
+STIM_W_PRE = (0., 2.)
+N_TRIALS_W_PRE = 200
+DIST_PRE = np.linspace(0, 0.3, 300)
+
+## V, G_N VS W_N_PC_EC, FR_EC COMPUTATION
+
+PATH_V_G_N_VS_W_N_PC_EC_FR_EC = 'search/pre/ridge/v_g_n_vs_w_n_pc_ec_fr_ec.npy'
+
+DUR_V_G_PRE = 21.
+MEASURE_START_V_G_PRE = 1.
+N_TIMEPOINTS_V_G_PRE = 1000
+
+W_N_PC_EC_PRE = np.linspace(0, 0.002, 200)
+FR_EC_PRE = np.linspace(20, 70, 100)
+
+
+# OBJECTIVE FUNCTION CONFIG
+
+N_NTWKS = 3
+SMLN_DUR = 0.2  # (s)
+MAX_RUNS_STABILIZE = 10
+
+RIDGE_H = 0.25  # (m)
+
+MIN_PC_FR_NZ_SGMS = 3  # minimum stds above bkgd fr to be considered nonzero
+MIN_FR_NZ = 1.  # (Hz)
+PPGN_WDW = 0.002  # (s)
+PPGN_LOOK_BACK = 2.  # 
+
+WAVE_TOL = 0.25
+LOOK_BACK_X = 2.
+
+T_FORCE = 0.001  # (s)
+N_L_PC_FORCE = 2.
+
+DECAY_WDW = (.5, .9)
+DECAY_RATIO = 0.95
+
+ACTIVITY_WDW = (.1, .9)
+SPEED_WDW = (.1, .9)
+
+
+# SEARCH CONFIG
+
+CONFIG_ROOT = 'search.config.ridge'
+MAX_SEARCH_ITER = 100000
+MAX_SEED = 10000
+WAIT_AFTER_ERROR = 5
