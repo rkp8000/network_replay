78fc468a4b9d8eea50b52bbcdcce36eea45e16f8
rkp8000 <rpang.contact@gmail.com>
2018-03-29 16:19:55 -0700

    Layout class/function structure.

---
+++ seq_replay/plot.py
@@ -0,0 +1,10 @@
+def raster():
+    pass
+
+
+def peak_rate_heatmap():
+    pass
+
+
+def excitability_profile():
+    pass
---
+++ seq_replay/smln.py
@@ -0,0 +1,19 @@
+def run():
+    pass
+
+
+def build_ntwk():
+    pass
+
+
+def build_stim():
+    pass
+
+
+def get_metrics():
+    pass
+
+
+def save():
+    pass
+
